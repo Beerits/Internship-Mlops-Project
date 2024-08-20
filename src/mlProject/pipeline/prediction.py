55297bc15911ebf6ad1bfb77b69a65d8dd02dcb7
Ritisha Deka <128310447+Beerits@users.noreply.github.com>
2024-08-20 16:24:56 +0530

    Complete project

--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -1,40 +1,4 @@
 
-# import joblib 
-# import numpy as np
-# import pandas as pd
-# import re
-# import nltk
-# from pathlib import Path
-# from nltk.corpus import stopwords
-# from sklearn.feature_extraction.text import CountVectorizer
-# from nltk.stem import PorterStemmer
-
-
-
-# class PredictionPipeline:
-    # def __init__(self):
-    #     self.model = joblib.load(Path('artifacts/model_trainer/model.joblib'))
-    #     self.vectorizer = joblib.load(Path('artifacts/model_trainer/vectorizer.joblib'))
-
-    
-    # def preprocess_text(self, text):
-    #     text = re.sub('<.*?>', '', text)
-    #     text = re.sub(r'https?://\S+', '', text)
-    #     text = re.sub(r'@\S+', '', text)
-    #     text = re.sub(r'#\S+', '', text)
-    #     text = re.sub('[^a-zA-Z]', ' ', text).lower()
-    #     words = nltk.word_tokenize(text)
-    #     words = [w for w in words if w not in stopwords.words('english')]
-    #     stemmer = PorterStemmer()
-    #     words = [stemmer.stem(w) for w in words]
-    #     text = ' '.join(words)
-    #     return text
-
-    # def predict(self, raw_data):
-    #     preprocessed_text = self.preprocess_text(raw_data)
-    #     vectorized_text = self.vectorizer.transform([preprocessed_text]).toarray()
-    #     prediction = self.model.predict(vectorized_text)
-    #     return prediction
 
 import joblib
 import numpy as np
